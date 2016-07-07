7f484d1956d324a8bdaa4ea9bf5444b0f8002ea9
kaufmann42 <kaufmann43@gmail.com>
2016-07-06 22:10:09 -0400

    added documentation

--- README.md
+++ README.md
@@ -1,7 +1,65 @@
 # globus-js
+
 A npm package for working with the globus API through HTTP REST calls
 
 ## Install
+
 npm install --save-dev globus-js
 
-## Documentation
+## API
+
+### getUserId
+
+getUserId - Given a token authorized by globus.org and a user's e-mail registered by globus
+it returns the user's id. Can be used in conjunction with shareEndpointWithUser's userId
+feild.
+
+**Parameters**
+
+-   `bearerToken` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** token authorized by globus.org
+-   `userEmail` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** User's e-mail
+
+Returns **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** User's UUID
+
+### shareEndpointWithUser
+
+shareEndpointWithUser - opens an access point with a given user. Shared endpointId's can be
+found by looking at the details of the endpoint you want to piggy back off of.
+
+**Parameters**
+
+-   `bearerToken` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** token authorized by globus.org
+-   `endpointId` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the id of the endpoint you'd like to base your share off of
+-   `userId` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the UUID of the user you'd like to share this endpoint with
+-   `path` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** an absolute path to the resoureces you'd like to share
+-   `userEmail` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the email of the user you'd like to notify
+-   `emailMessage` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the message you'd like to attach to the e-mail
+
+Returns **[boolean](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean)** true if successful and false if otherwise
+
+### getEndPoint
+
+getEndPoint - get's information about an endpoint given its endpointId.
+
+**Parameters**
+
+-   `bearerToken` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** token authorized by globus.org
+-   `endpointId` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the id of the endpoint you'd like to base your share off of
+
+Returns **[object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object)** a JSON object of the representation of the endpoint
+
+### createEndPoint
+
+createEndPoint - creates a shared_endpoint endpoint that it's ACL can be editted
+to share paths with certain people.
+
+**Parameters**
+
+-   `bearerToken` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** token authorized by globus.org
+-   `displayName` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** display name of the endpoint
+-   `hostId` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the Id of the host endpoint that this endpoint will append
+-   `path` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** an absolute path to the resoureces you'd like to share
+-   `description` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** a short description of the endpoint
+-   `organization` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** the organization that is opening this endpoint
+
+Returns **[boolean](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean)** returns true if successful
--- globus.js
+++ globus.js
@@ -2,8 +2,19 @@ var request = require('request'),
     transferBaseURL = 'https://transfer.api.globusonline.org/v0.10',
     authBaseURL = 'https://auth.globus.org/v2/api/';
 
+
+/**
+ * getUserId - Given a token authorized by globus.org and a user's e-mail registered by globus
+ * it returns the user's id. Can be used in conjunction with shareEndpointWithUser's userId
+ * feild.
+ *
+ * @param  {string} bearerToken token authorized by globus.org
+ * @param  {string} userEmail   User's e-mail
+ * @return {string}             User's UUID
+ */
 exports.getUserId = function(bearerToken, userEmail) {
     var url = authBaseURL + 'identities?usernames=' + userEmail.replace('@', '%40');
+
     function callback(err, response, body) {
         if (err) {
             console.error(err);
@@ -11,51 +22,103 @@ exports.getUserId = function(bearerToken, userEmail) {
         }
         return JSON.parse(body).identities[0].id;
     }
-    request.get(url, callback).auth(null, null, true, bearerToken);
-}
 
+    request.get(url, callback).auth(null, null, true, bearerToken);
+};
+
+
+/**
+ * shareEndpointWithUser - opens an access point with a given user. Shared endpointId's can be
+ * found by looking at the details of the endpoint you want to piggy back off of.
+ *
+ * @param  {string} bearerToken  token authorized by globus.org
+ * @param  {string} endpointId   the id of the endpoint you'd like to base your share off of
+ * @param  {string} userId       the UUID of the user you'd like to share this endpoint with
+ * @param  {string} path         an absolute path to the resoureces you'd like to share
+ * @param  {string} userEmail    the email of the user you'd like to notify
+ * @param  {string} emailMessage the message you'd like to attach to the e-mail
+ * @return {boolean}             true if successful and false if otherwise
+ */
 exports.shareEndpointWithUser = function(bearerToken, endpointId, userId, path, userEmail, emailMessage) {
-    var url = transferBaseURL + '/endpoint/' + endpointId + '/access'
-    acl_json = {
-        json: {
-            'DATA_TYPE': 'access',
-            'principal_type': 'identity',
-            'principal': userId,
-            'path': path,
-            'permissions': 'r',
-            'notify_email': userEmail,
-            'notify_message': emailMessage
+    var url = transferBaseURL + '/endpoint/' + endpointId + '/access',
+        acl_json = {
+            json: {
+                'DATA_TYPE': 'access',
+                'principal_type': 'identity',
+                'principal': userId,
+                'path': path,
+                'permissions': 'r',
+                'notify_email': userEmail,
+                'notify_message': emailMessage
+            }
+        };
+
+    function callback(err, response, body) {
+        if (err) {
+            console.error(err);
+            return false;
         }
+        return true;
     }
 
     request.post(url, acl_json, callback).auth(null, null, true, bearerToken);
-}
+};
 
+
+/**
+ * getEndPoint - get's information about an endpoint given its endpointId.
+ *
+ * @param  {string} bearerToken token authorized by globus.org
+ * @param  {string} endpointId  the id of the endpoint you'd like to base your share off of
+ * @return {object}             a JSON object of the representation of the endpoint
+ */
 exports.getEndPoint = function(bearerToken, endpointId) {
-    var url = transferBaseURL + '/' + endpointId
-    request.get(url, callback).auth(null, null, true, bearerToken);
-}
-
-exports.createEndPoint = function(bearerToken, displayName, hostId, path, description, organization) {
-    var url = transferBaseURL + '/endpoint'
-    shared_endpoint_json = {
-        json: {
-            'DATA_TYPE': 'shared_endpoint',
-            'display_name': displayName,
-            'host_endpoint': hostId,
-            'host_path': path,
-            'description': description,
-            'organization': organization
-        }
-    }
+    var url = transferBaseURL + '/' + endpointId;
 
     function callback(err, response, body) {
         if (err) {
             console.error(err);
             return;
         }
-        console.log(body);
+        return body;
+    }
+
+    request.get(url, callback).auth(null, null, true, bearerToken);
+};
+
+
+/**
+ * createEndPoint - creates a shared_endpoint endpoint that it's ACL can be editted
+ * to share paths with certain people.
+ *
+ * @param  {string} bearerToken  token authorized by globus.org
+ * @param  {string} displayName  display name of the endpoint
+ * @param  {string} hostId       the Id of the host endpoint that this endpoint will append
+ * @param  {string} path         an absolute path to the resoureces you'd like to share
+ * @param  {string} description  a short description of the endpoint
+ * @param  {string} organization the organization that is opening this endpoint
+ * @return {boolean}             returns true if successful
+ */
+exports.createEndPoint = function(bearerToken, displayName, hostId, path, description, organization) {
+    var url = transferBaseURL + '/endpoint',
+        shared_endpoint_json = {
+            json: {
+                'DATA_TYPE': 'shared_endpoint',
+                'display_name': displayName,
+                'host_endpoint': hostId,
+                'host_path': path,
+                'description': description,
+                'organization': organization
+            }
+        };
+
+    function callback(err, response, body) {
+        if (err) {
+            console.error(err);
+            return false;
+        }
+        return true;
     }
 
     request.post(url, shared_endpoint_json, callback).auth(null, null, true, bearerToken);
-}
+};
